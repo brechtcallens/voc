a181d12c13eca6cd41cb928c8e980b79a10024fa
Manuel Geiger <manuel.geiger@tum.de>
2021-09-06 10:48:45 +0200

    Add more test cases to sqrt unit test

--- tests/stdlib/test_math.py
+++ tests/stdlib/test_math.py
@@ -4,7 +4,26 @@ from ..utils import TranspileTestCase
 class MathModuleTests(TranspileTestCase):
 
     def test_sqrt(self):
-        self.assertCodeExecution("""
-            import math
-            print(math.sqrt(4))
-        """)
+        def create_test(arg, expect_exception=False):
+            if expect_exception:
+                return f"""
+                    import math
+                    try:
+                        print(math.sqrt({arg}))
+                    except Exception as e:
+                        print(type(e), ':', e)
+                """
+            else:
+                return f"""
+                    import math
+                    print(math.sqrt({arg}))
+                """
+
+        self.assertCodeExecution(create_test("4"))
+        self.assertCodeExecution(create_test("12.34"))
+        self.assertCodeExecution(create_test("-3.5", expect_exception=True))
+        self.assertCodeExecution(create_test("-0.0"))
+        self.assertCodeExecution(create_test("float('inf')"))
+        self.assertCodeExecution(create_test("float('nan')"))
+        self.assertCodeExecution(create_test("float('-inf')", expect_exception=True))
+        self.assertCodeExecution(create_test("'Hello'", expect_exception=True))
