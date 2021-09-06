eb23896e028954998af12dc2ea5640909ac13d5b
Manuel Geiger <manuel.geiger@tum.de>
2021-09-06 11:46:39 +0200

    Remove unnecessary expect_exception parameter for test cases
    We don't want to test whether a certain argument raises an error,
    but only that python and java behave the same way. Thus, it is better
    to just always catch exceptions.

--- tests/stdlib/test_math.py
+++ tests/stdlib/test_math.py
@@ -4,26 +4,20 @@ from ..utils import TranspileTestCase
 class MathModuleTests(TranspileTestCase):
 
     def test_sqrt(self):
-        def create_test(arg, expect_exception=False):
-            if expect_exception:
-                return f"""
-                    import math
-                    try:
-                        print(math.sqrt({arg}))
-                    except Exception as e:
-                        print(type(e), ':', e)
-                """
-            else:
-                return f"""
-                    import math
+        def create_test(arg):
+            return f"""
+                import math
+                try:
                     print(math.sqrt({arg}))
-                """
+                except Exception as e:
+                    print(type(e), ':', e)
+            """
 
         self.assertCodeExecution(create_test("4"))
         self.assertCodeExecution(create_test("12.34"))
-        self.assertCodeExecution(create_test("-3.5", expect_exception=True))
+        self.assertCodeExecution(create_test("-3.5"))
         self.assertCodeExecution(create_test("-0.0"))
         self.assertCodeExecution(create_test("float('inf')"))
         self.assertCodeExecution(create_test("float('nan')"))
-        self.assertCodeExecution(create_test("float('-inf')", expect_exception=True))
-        self.assertCodeExecution(create_test("'Hello'", expect_exception=True))
+        self.assertCodeExecution(create_test("float('-inf')"))
+        self.assertCodeExecution(create_test("'Hello'"))
