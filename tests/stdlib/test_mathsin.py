328ce6420e34f7ecbb5de60a1e201b6612056f28
Afixe <Afix94@gmail.com>
2021-09-07 11:56:33 +0200

    stylechcek

--- tests/stdlib/test_mathsin.py
+++ tests/stdlib/test_mathsin.py
@@ -6,49 +6,39 @@ class MathSinTests(TranspileTestCase):
     def test_sin_zero(self):
         self.assertCodeExecution("""
             from math import sin
-            print(sin(0)) 
-
+            print(sin(0))
             print(sin(0.0))
-        """
-        )
+        """)
 
     def test_sin_positive__int_values(self):
 
         self.assertCodeExecution("""
             from math import sin
             print(sin(5))
-        """
-        )
+        """)
 
     def test_sin_negative__int_values(self):
 
         self.assertCodeExecution("""
             from math import sin
             print(sin(-5))
-        """
-        )
+        """)
 
     def test_sin_postive_float_values(self):
         self.assertCodeExecution("""
             from math import sin
             print(sin(5.0))
-        """
-        )
+        """)
 
     def test_sin_negative_float_values(self):
         self.assertCodeExecution("""
             from math import sin
             print(sin(-5.0))
-        """
-        )
+        """)
 
-    
     def test_sin_boolean_boolean_values(self):
         self.assertCodeExecution("""
             from math import sin
             print(sin(True))
             print(sin(False))
-        """
-        )
-    
-    
+        """)
