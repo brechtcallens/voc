365413d4231372d6dd057d42b6c622290fbc65b8
Brecht Callens <brecalle.callens@ugent.be>
2021-09-07 15:28:37 +0200

    Added test cases for integer and string arguments

--- stdlib_tests/test_math.py
+++ stdlib_tests/test_math.py
@@ -3,3 +3,7 @@ import math
 print(math.fabs(-1.4))
 print(math.fabs(0.0))
 print(math.fabs(1.4))
+
+print(math.fabs(-1))
+print(math.fabs(0))
+print(math.fabs(1))
--- tests/stdlib/test_math.py
+++ tests/stdlib/test_math.py
@@ -1,21 +1,48 @@
+from unittest import expectedFailure
+
 from ..utils import TranspileTestCase
 
 
 class MathModuleTests(TranspileTestCase):
-    def test_fabs_negative(self):
+    def test_fabs_float_negative(self):
         self.assertCodeExecution("""
             import math
             print(math.fabs(-1.4))
             """)
 
-    def test_fabs_zero(self):
+    def test_fabs_float_zero(self):
         self.assertCodeExecution("""
             import math
             print(math.fabs(0.0))
             """)
 
-    def test_fabs_positive(self):
+    def test_fabs_float_positive(self):
         self.assertCodeExecution("""
             import math
             print(math.fabs(1.4))
+            """)
+    
+    def test_fabs_integer_negative(self):
+        self.assertCodeExecution("""
+            import math
+            print(math.fabs(-1))
+            """)
+
+    def test_fabs_integer_zero(self):
+        self.assertCodeExecution("""
+            import math
+            print(math.fabs(0))
+            """)
+
+    def test_fabs_integer_positive(self):
+        self.assertCodeExecution("""
+            import math
+            print(math.fabs(1))
+            """)
+
+    @expectedFailure
+    def test_fabs_string(self):
+        self.assertCodeExecution("""
+            import math
+            print(math.fabs('string'))
             """)
