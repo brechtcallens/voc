db590ba2703eed682f3912b630c992379d0022bd
Manuel Geiger <manuel.geiger@tum.de>
2021-09-08 15:51:21 +0200

    Add math module documentation

--- python/common/python/math.java
+++ python/common/python/math.java
@@ -1,6 +1,26 @@
 package python;
 
-@org.python.Module()
+@org.python.Module(
+    __doc__ =
+            "This module provides access to the mathematical functions " +
+            "defined by the C standard." +
+            "\n" +
+            "These functions cannot be used with complex numbers; use " +
+            "the functions of the same name from the cmath module if " +
+            "you require support for complex numbers. The distinction " +
+            "between functions which support complex numbers and those " +
+            "which don’t is made since most users do not want to learn " +
+            "quite as much mathematics as required to understand " +
+            "complex numbers. Receiving an exception instead of a " +
+            "complex result allows earlier detection of the unexpected " +
+            "complex number used as a parameter, so that the programmer " +
+            "can determine how and why it was generated in the first " +
+            "place." +
+            "\n" +
+            "The following functions are provided by this module. " +
+            "Except when explicitly noted otherwise, all return values " +
+            "are floats."
+)
 public class math extends org.python.types.Module {
 
     @org.python.Method(
