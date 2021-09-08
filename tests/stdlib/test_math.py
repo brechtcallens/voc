1e9a7e47e733326ee8761caf91daa22ccdaccb54
Manuel Geiger <manuel.geiger@tum.de>
2021-09-08 16:13:48 +0200

    Split unit tests into multiple methods

--- tests/stdlib/test_math.py
+++ tests/stdlib/test_math.py
@@ -3,21 +3,33 @@ from ..utils import TranspileTestCase
 
 class MathModuleTests(TranspileTestCase):
 
-    def test_sqrt(self):
-        def create_test(arg):
-            return f"""
-                import math
-                try:
-                    print(math.sqrt({arg}))
-                except Exception as e:
-                    print(type(e), ':', e)
-            """
+    @staticmethod
+    def create_test(func_name, arg_list):
+        return f"""
+            import math
+            try:
+                print(math.{func_name}({arg_list}))
+            except Exception as e:
+                print(type(e), ':', e)
+        """
 
-        self.assertCodeExecution(create_test("4"))
-        self.assertCodeExecution(create_test("12.34"))
-        self.assertCodeExecution(create_test("-3.5"))
-        self.assertCodeExecution(create_test("-0.0"))
-        self.assertCodeExecution(create_test("float('inf')"))
-        self.assertCodeExecution(create_test("float('nan')"))
-        self.assertCodeExecution(create_test("float('-inf')"))
-        self.assertCodeExecution(create_test("'Hello'"))
+    @staticmethod
+    def create_test_sqrt(arg):
+        return MathModuleTests.create_test("sqrt", arg)
+
+    def test_sqrt_valid_values(self):
+        self.assertCodeExecution(self.create_test_sqrt("4"))
+        self.assertCodeExecution(self.create_test_sqrt("12.34"))
+        self.assertCodeExecution(self.create_test_sqrt("-0.0"))
+
+    def test_sqrt_invalid_values(self):
+        self.assertCodeExecution(self.create_test_sqrt("-3.5"))
+        self.assertCodeExecution(self.create_test_sqrt("-5"))
+
+    def test_sqrt_special_values(self):
+        self.assertCodeExecution(self.create_test_sqrt("float('inf')"))
+        self.assertCodeExecution(self.create_test_sqrt("float('nan')"))
+        self.assertCodeExecution(self.create_test_sqrt("float('-inf')"))
+
+    def test_sqrt_invalid_type(self):
+        self.assertCodeExecution(self.create_test_sqrt("'Hello'"))
