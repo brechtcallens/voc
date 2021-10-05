fcb217ed296d072fce83dc3bac50492b9be35341
Ectras <40306539+Ectras@users.noreply.github.com>
2021-10-06 00:53:31 +0200

    Add datetime module transpile test for attribute accessing
    The write tests fail as the correct Java behavior is not implemented yet.

---
+++ tests/stdlib/test_datetime.py
@@ -0,0 +1,33 @@
+from ..utils import TranspileTestCase
+
+
+class DateTimeModuleTests(TranspileTestCase):
+
+    @staticmethod
+    def create_date_test(command):
+        return f"""
+            import datetime
+            try:
+                date = datetime.date(year=2021, month=10, day=1)
+                {command}
+            except Exception as e:
+                print(type(e), ':', e)
+        """
+
+    def test_read_year(self):
+        self.assertCodeExecution(self.create_date_test("print(date.year)"))
+
+    def test_write_year(self):
+        self.assertCodeExecution(self.create_date_test("date.year = 2016"))
+
+    def test_read_month(self):
+        self.assertCodeExecution(self.create_date_test("print(date.month)"))
+
+    def test_write_month(self):
+        self.assertCodeExecution(self.create_date_test("date.month = 6"))
+
+    def test_read_day(self):
+        self.assertCodeExecution(self.create_date_test("print(date.day)"))
+
+    def test_write_day(self):
+        self.assertCodeExecution(self.create_date_test("date.day = 20"))
